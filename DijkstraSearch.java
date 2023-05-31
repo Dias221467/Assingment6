f81f695034986ca95fc524977d90f4c6034483e8
Диас <lbrjyz05@mail.ru>
2023-06-01 01:53:20 +0600

    12

--- DijkstraSearch.java
+++ DijkstraSearch.java
@@ -1,3 +1,19 @@
 import java.util.Map;
-public class DijkstraSearch {
+
+public class DijkstraSearch<V> implements Search<V> {
+    private WeightedGraph<V> graph;
+
+    public DijkstraSearch(WeightedGraph<V> graph) {
+        this.graph = graph;
+    }
+
+    @Override
+    public void search(Vertex<V> start) {
+        Map<Vertex<V>, Double> distances = graph.Dijkstra(start);
+        for (Map.Entry<Vertex<V>, Double> entry : distances.entrySet()) {
+            Vertex<V> vertex = entry.getKey(); // Get the vertex
+            double distance = entry.getValue(); // Get the distance
+            System.out.println("Vertex " + vertex.getData() + ": " + distance);
+        }
+    }
 }
