24aac955e08712562f9944928b69c2b820843c57
Диас <lbrjyz05@mail.ru>
2023-06-01 01:14:32 +0600

    9

---
+++ BreadthFirstSearch.java
@@ -0,0 +1,12 @@
+public class BreadthFirstSearch<V> implements Search<V> {
+    private WeightedGraph<V> graph;
+
+    public BreadthFirstSearch(WeightedGraph<V> graph) {
+        this.graph = graph;
+    }
+
+    @Override
+    public void search(Vertex<V> start) {
+        graph.BFS(start);
+    }
+}
