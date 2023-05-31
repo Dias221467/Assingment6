4d37fe7644472aac0cf779f70bef592ad9664800
Диас <lbrjyz05@mail.ru>
2023-06-01 00:53:32 +0600

    4

--- WeightedGraph.java
+++ WeightedGraph.java
@@ -9,4 +9,18 @@ public class WeightedGraph<V>{
     public void addVertex(Vertex<V> vertex) {
         adjacencyList.put(vertex, new LinkedList<>());
     }
+    public void addEdge(Vertex<V> source, Vertex<V> destination, double weight) {
+        validateVertex(source);
+        validateVertex(destination);
+
+        source.addAdjacentVertex(destination, weight);
+        destination.addAdjacentVertex(source, weight);
+
+        adjacencyList.get(source).add(destination);
+        adjacencyList.get(destination).add(source);
+    }
+    private void validateVertex(Vertex<V> vertex) {
+        if (!adjacencyList.containsKey(vertex))
+            throw new IllegalArgumentException("Vertex " + vertex + " is not in the graph");
+    }
 }
